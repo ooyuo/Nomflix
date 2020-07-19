8b4f8a150c4763b3e311f6e170ba402cc895fee6
doyounjeong <doyounjeong0@gmail.com>
2020-07-19 21:35:27 +0900

    #2.2 React Router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <>
+    <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-    </>
+      <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />{" "}
+      {/* 일치하는 route가 하나도 없다면 /로 보낸다. */}
+    </Switch>
   </Router>
 );
