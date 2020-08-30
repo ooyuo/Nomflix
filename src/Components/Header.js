6f050a71b3f6f8155a6fd4483c6aaa7a5ec12916
doyounjeong <doyounjeong0@gmail.com>
2020-08-30 21:32:43 +0900

    #3.4 Location Aware Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,9 +11,8 @@ const Header = styled.header`
   height: 50px;
   display: flex;
   align-items: center;
-  padding: 0px 10px;
-  z-index: 10px;
   background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
@@ -25,8 +24,9 @@ const Item = styled.li`
   width: 80px;
   height: 50px;
   text-align: center;
-  border-bottom: 5px solid
+  border-bottom: 3px solid
     ${(props) => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -36,21 +36,18 @@ const SLink = styled(Link)`
   justify-content: center;
 `;
 
-export default withRouter((props) => {
-  const pathname = "";
-  return (
-    <Header>
-      <List>
-        <Item current={pathname === "/"}>
-          <SLink to="/">Movies</SLink>
-        </Item>
-        <Item current={pathname === "/tv"}>
-          <SLink to="/tv">TV</SLink>
-        </Item>
-        <Item current={pathname === "/search"}>
-          <SLink to="/search">Search</SLink>
-        </Item>
-      </List>
-    </Header>
-  );
-});
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
+    <List>
+      <Item current={pathname === "/"}>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item current={pathname === "/tv"}>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item current={pathname === "/search"}>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+));
